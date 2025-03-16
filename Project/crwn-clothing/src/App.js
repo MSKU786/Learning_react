191319095e551e5bb91e84c4b87eeaccc7ac1c11
MSKU786 <manishsingh332000@gmail.com>
2025-03-16 16:39:11 +0530

    Added navigation routes

--- Project/crwn-clothing/src/App.js
+++ Project/crwn-clothing/src/App.js
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
   return (
     <Routes>
       <Route
         path="/"
-        element={<Home />}
-      />
+        element={<Navigation />}
+      >
+        <Route
+          index
+          element={<Home />}
+        />
+      </Route>
     </Routes>
   );
 };
