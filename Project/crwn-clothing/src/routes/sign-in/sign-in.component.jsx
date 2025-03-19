0a09b5886dc4bf9249b11c5498d9fdd3705d3307
MSKU786 <manishsingh332000@gmail.com>
2025-03-19 11:34:30 +0530

    Added createUserDocumeent function in signin method

--- Project/crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ Project/crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,19 @@
+import {
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+} from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+
+    createUserDocumentFromAuth(response);
+  };
   return (
     <div>
       <h2> Sign In </h2>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
     </div>
   );
 };
--- Project/crwn-clothing/src/utils/firebase/firebase.utils.js
+++ Project/crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -1,4 +1,10 @@
-import { initializeApp } from 'firebase/app';
+import {
+  initializeApp,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+} from 'firebase/app';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.apiKey,
@@ -10,3 +16,38 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomerParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = await doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (userSnapshot.exists()) {
+    return userDocRef;
+  }
+
+  const { displayName, email } = userAuth;
+  const createdAt = new Date();
+
+  try {
+    await setDoc(userDocRef, {
+      displayName,
+      email,
+      createdAt,
+    });
+  } catch (err) {
+    console.log('error createing the user', err.message);
+  }
+
+  return userDocRef;
+};
