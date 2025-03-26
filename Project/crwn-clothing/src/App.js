16029bedd6384193fbbc55f7e8251945e13cfd23
MSKU786 <manishsingh332000@gmail.com>
2025-03-26 10:59:13 +0530

    Started with shop page

--- Project/crwn-clothing/src/App.js
+++ Project/crwn-clothing/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import SignIn from './routes/sign-in/sign-in.component';
+import { Shop } from './components/shop/shop.component';
 
 const App = () => {
   return (
@@ -13,6 +14,10 @@ const App = () => {
         <Route
           index
           element={<Home />}
+        />
+        <Route
+          path="/shop"
+          element={<Shop />}
         />
         <Route
           path="/sign-in"
