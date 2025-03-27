8f74abdb1c2a50a3ec916f23697a9d2a5c4586d0
MSKU786 <manishsingh332000@gmail.com>
2025-03-27 10:36:40 +0530

    Added product card component

--- Project/crwn-clothing/src/components/shop/shop.component.jsx
+++ Project/crwn-clothing/src/components/shop/shop.component.jsx
@@ -1,14 +1,17 @@
 import { useContext } from 'react';
-
+import './shop.component.scss';
 import { ProductContext } from '../../contexts/products.context';
+import { ProductCard } from '../product-card/product-card.component';
+
 export const Shop = () => {
   const { products } = useContext(ProductContext);
   return (
-    <div>
-      {products.map(({ id, name }) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
       ))}
     </div>
   );
