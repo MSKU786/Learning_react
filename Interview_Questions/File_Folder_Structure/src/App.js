d806976594ced931ec0022c25bcf60eaad650e83
MSKU786 <manishsingh332000@gmail.com>
2025-04-01 10:32:53 +0530

    Inverview question for file folder structure UI

---
+++ Interview_Questions/File_Folder_Structure/src/App.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+// Problem Statement: Create a file folder structure
+export default function App() {
+  return (
+    <div className="App">
+      <h1>file folder structure</h1>
+    </div>
+  );
+}
