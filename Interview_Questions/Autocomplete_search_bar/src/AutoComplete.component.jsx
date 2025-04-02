decbae440d32692b1205aa472b5d8bab9eb06fae
MSKU786 <manishsingh332000@gmail.com>
2025-04-02 11:39:50 +0530

    added some optimization

--- Interview_Questions/Autocomplete_search_bar/src/AutoComplete.component.jsx
+++ Interview_Questions/Autocomplete_search_bar/src/AutoComplete.component.jsx
@@ -1,34 +1,31 @@
 import { useEffect, useState } from 'react';
+import './styles.css';
 
 export const AutoComplete = () => {
   const [search, setSearch] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
   const [filteredSuggestions, setFilteredSuggestions] = useState([]);
 
   const handleChange = (e) => {
     const value = e.target.value;
     setSearch(value);
+  };
 
-    const filterSuggestions = suggestions.filter((suggest) =>
-      suggest.toLowerCase().includes(value.toLowerCase())
+  const fetchRecipes = async () => {
+    const results = await fetch(
+      `https://dummyjson.com/recipes/search?q=${search}`
     );
-    setFilteredSuggestions(filterSuggestions);
+    const data = await results.json();
+
+    const recipes = data?.recipes?.map((item) => item.name);
+    setFilteredSuggestions(recipes);
   };
 
   useEffect(() => {
-    const fetchRecipes = async () => {
-      const results = await fetch(`https://dummyjson.com/recipes`);
-      const data = await results.json();
-
-      const recipes = data?.recipes?.map((item) => item.name);
-      console.log(recipes);
-      setSuggestions(recipes);
-    };
     fetchRecipes();
-  }, []);
+  }, [search]);
 
   return (
-    <div>
+    <div className="autocomplete-container">
       <input
         id="search-input"
         type="text"
@@ -36,10 +33,18 @@ export const AutoComplete = () => {
         value={search}
       />
 
-      {search && (
-        <div>
+      {filteredSuggestions.length > 0 && (
+        <div className="autocomplete-results">
           {filteredSuggestions.map((suggestion) => {
-            return <div>{suggestion}</div>;
+            return (
+              <div
+                className="suggestion-result"
+                key={suggestion}
+                onClick={() => setSearch(suggestion)}
+              >
+                {suggestion}
+              </div>
+            );
           })}
         </div>
       )}
