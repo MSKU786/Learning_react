8eba50866617f09e53b4300d397e62fdbbf992db
MSKU786 <manishsingh332000@gmail.com>
2025-04-02 10:52:27 +0530

    AutoComplet basic functionality completed

--- Interview_Questions/Autocomplete_search_bar/src/App.js
+++ Interview_Questions/Autocomplete_search_bar/src/App.js
@@ -1,10 +1,11 @@
-import { ProgresBar } from './ProgresBar.component';
+import { AutoComplete } from './AutoComplete.component';
 import './styles.css';
 
 export default function App() {
   return (
     <div className="App">
       <h1>AutComplete Search Bar</h1>
+      <AutoComplete />
     </div>
   );
 }
--- Interview_Questions/Autocomplete_search_bar/src/AutoComplete.component.jsx
+++ Interview_Questions/Autocomplete_search_bar/src/AutoComplete.component.jsx
@@ -3,32 +3,46 @@ import { useEffect, useState } from 'react';
 export const AutoComplete = () => {
   const [search, setSearch] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
 
   const handleChange = (e) => {
     const value = e.target.value;
     setSearch(value);
+
+    const filterSuggestions = suggestions.filter((suggest) =>
+      suggest.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredSuggestions(filterSuggestions);
   };
 
-  useEffect(async () => {
-    const results = await fetch(`https://dummyjson.com/recipes`);
-    const data = await results.json()?.recipes;
-    const recipes = data.map((item) => item.name);
-    console.log(recipes);
-    setSuggestions(recipes);
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const results = await fetch(`https://dummyjson.com/recipes`);
+      const data = await results.json();
+
+      const recipes = data?.recipes?.map((item) => item.name);
+      console.log(recipes);
+      setSuggestions(recipes);
+    };
+    fetchRecipes();
   }, []);
+
   return (
     <div>
       <input
         id="search-input"
         type="text"
         onChange={handleChange}
+        value={search}
       />
 
-      <div>
-        {suggestions.map((suggestion) => {
-          return <div>{suggestion}</div>;
-        })}
-      </div>
+      {search && (
+        <div>
+          {filteredSuggestions.map((suggestion) => {
+            return <div>{suggestion}</div>;
+          })}
+        </div>
+      )}
     </div>
   );
 };
