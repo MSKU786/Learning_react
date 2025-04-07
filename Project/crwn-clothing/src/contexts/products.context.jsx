9033129060e615db8a46dae0b5e03dc937f91a90
MSKU786 <manishsingh332000@gmail.com>
2025-04-07 10:47:51 +0530

    Adding data to firesbase with useEffect one time task

--- Project/crwn-clothing/src/contexts/products.context.jsx
+++ Project/crwn-clothing/src/contexts/products.context.jsx
@@ -1,13 +1,22 @@
-import { createContext, useState } from 'react';
-import PRODUCTS from '../shop-data.json';
+import { createContext, useEffect, useState } from 'react';
+import SHOP_DATA from '../shop-data.json';
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const ProductContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = { products, setProducts };
+
+  useEffect(() => {
+    const getProducts = async () => {
+      await addCollectionAndDocuments('products', SHOP_DATA);
+    };
+
+    getProducts();
+  }, []);
 
   return (
     <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
