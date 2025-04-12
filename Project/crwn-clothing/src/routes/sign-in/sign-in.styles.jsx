36303305dabd17449d6cf3f84a984dfe2b64fbdb
MSKU786 <manishsingh332000@gmail.com>
2025-04-12 12:26:36 +0530

    Button as an sytled component

---
+++ Project/crwn-clothing/src/routes/sign-in/sign-in.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AuthContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 900px;
+  margin: 30px auto;
+`;
