fe6f494ea3c9fc02fbe5842717c7020ea3e01916
MSKU786 <manishsingh332000@gmail.com>
2025-05-12 11:16:03 +0530

    Adding dark and light mode interview question

---
+++ Interview_Questions/dark_and_light_mode/src/ProgresBar.component.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const ProgresBar = ({ progress }) => {
+  return (
+    <div className="progress-bar">
+      <span
+        className="filled"
+        style={{ width: `${progress}%` }}
+      >{`${progress}%`}</span>
+    </div>
+  );
+};
